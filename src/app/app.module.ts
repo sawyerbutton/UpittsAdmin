eb169096090ae821c50afc6303a03af9581ae93f
sawyerbutton <lil103@pitt.edu>
2018-02-26 14:38:36 -0500

    fix the module problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -20,18 +19,12 @@ import { SysLoginComponent } from './login/sys-login/sys-login.component';
 import { BhcoLoginComponent } from './login/non-sys-login/bhco-login/bhco-login.component';
 import { MemberLoginComponent } from './login/non-sys-login/member-login/member-login.component';
 import { ForgotPwdComponent } from './login/forgot-pwd/forgot-pwd.component';
-=======
-import { BasicComponent } from './basic/basic.component';
-import { LoginComponent} from './login/login.component';
-import { LayoutModule} from './layout/layout/layout.module';
-import {LayoutComponent} from './layout/layout.component';
->>>>>>> 8e4f936fff4c4299dc1f916aef8c3b876daf7ce5
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
     SysLoginComponent,
     BhcoLoginComponent,
@@ -50,17 +43,7 @@ import {LayoutComponent} from './layout/layout.component';
     MatStepperModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    BrowserAnimationsModule
-=======
-    BasicComponent,
-    LoginComponent,
-    LayoutComponent
-  ],
-  imports: [
-    BrowserModule,
     BrowserAnimationsModule,
-    LayoutModule
->>>>>>> 8e4f936fff4c4299dc1f916aef8c3b876daf7ce5
   ],
   providers: [],
   bootstrap: [AppComponent]
